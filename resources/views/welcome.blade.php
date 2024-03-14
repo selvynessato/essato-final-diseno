26eb9413679273f1ae6a41178adf6708454603e6
Selvyn Quijivix <selvyn.essato@gmail.com>
2024-03-13 18:08:21 -0600

    se hizo el header y primera parte video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,40 +14,111 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
 </head>
 <body>
     <header>
-        <ul class="nav justify-content-end">
-            @if (Route::has('login'))
-                <li class="nav-item">
-                    <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">
-                        <img src="{{asset('/images/navbar/logo-rojo.png')}}" alt="Logo" width="64" height="64" class="d-inline-block align-text-top">
-                        </a>
-                    </div>
-                        @auth
-                            <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 nav-link">Inicio</a>
-                        @else
-                            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 nav-link">Log in</a>
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 nav-link">Register</a>
-                            @endif
-                        @endauth
-                    </div>
-                </li>
-            @endif
-        </ul>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                <img src="{{asset('/images/navbar/logo-essato-blanco.png')}}" alt="Logo" width="50" height="50" class="d-inline-block align-text-top">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    @if (Route::has('login'))
+                    @auth
+                        <li class="nav-item">
+                            <a href="{{ url('/home') }}" class="nav-link">Inicio</a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a href="{{ route('login') }}" class="nav-link">
+                                <img src="{{asset('/images/navbar/cliente.png')}}" alt="logo cliente" width="32" height="32">
+                            </a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <!--a href="{{ route('register') }}" class="nav-link">Register</a-->
+                            <a href="#" class="nav-link">
+                                <img src="{{asset('/images/navbar/escuela.png')}}" alt="logo cliente" width="32" height="32">
+                            </a>
+                        </li>
+                        @endif
+                    @endauth
+                    @endif
+                </ul>
+                </div>
+            </div>
+        </nav>
     </header>
 
     <main>
+        <div class="primera-parte">
+            <div class="texto-inicio">     
+                <h1>ESSATO</h1>
+                <h2 id="subtitulo">Somos más que una agencia de marketing,</h2>
+                <h2>Somos una agencia de progreso y avance para tu negocio.</h2>
+            </div>      
+
+            <div class="primera-video">
+                <iframe width="100%" height="400" src="https://www.youtube.com/embed/XBaVaoGIEbc?si=hmi2R4HLKE1jAw0i" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+        
+            <div data-aos="fade-right"> 
+                <img src="{{asset('/images/facebook.png')}}" alt="Logo" class="d-inline-block align-text-top">
+            </div>
+
+            <div data-aos="fade-left">
+                <img src="{{asset('/images/tik-tok.png')}}" alt="Logo"  class="d-inline-block align-text-top">
+            </div>
+        </div>
     </main>
 
-    <footer>
-    </footer>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-3 mb-md-0">
+                <div class="social-icons">
+                    <div class="social-icon">
+                        <a href="https://www.facebook.com/essatomarketing" target="_blank">
+                            <img src="{{asset('/images/facebook.png')}}" class="img-fluid" alt="facebook">
+                        </a>
+                    </div>
+                    <div class="social-icon">
+                        <a href="https://www.instagram.com/essatomarketing/" target="_blank">
+                            <img src="{{asset('/images/instagram.png')}}" class="img-fluid" alt="instagram">
+                        </a>
+                    </div>
+                    <div class="social-icon">
+                        <a href="https://www.tiktok.com/@essato_marketing" target="_blank">
+                            <img src="{{asset('/images/tik-tok.png')}}" class="img-fluid" alt="tik-tok">
+                        </a>
+                    </div>
+                    <div class="social-icon">
+                        <a href="https://www.youtube.com/@essato_marketing" target="_blank">
+                            <img src="{{asset('/images/youtube.png')}}" class="img-fluid" alt="youtube">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- Espacio entre las columnas -->
+            <div class="col-md-2 d-none d-md-block" style="border:none;"></div> 
+            <div class="col-md-6">
+                <p class="text-center text-md-end">&copy; Copyringht Essato Marketing y Publicidad - 2024</p>
+            </div>
+        </div>
+    </div>
+</footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
 </body>
 </html>
