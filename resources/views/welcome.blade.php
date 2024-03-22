50707e598b785e1844ab5ef2db3ae5e9961bcdfa
Selvyn Quijivix <selvyn.essato@gmail.com>
2024-03-22 00:22:32 -0600

    principal 95% blog, falta footer, formulario y boton encabezado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -406,31 +406,8 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
-    <script>
-        $(document).ready(function() {
-            $('.owl-carousel').owlCarousel({
-                loop:true,
-                margin:10,
-                nav:true,
-                autoplay:true, 
-                autoplayTimeout: 1500, 
-                autoplayHoverPause:true,
-                responsive:{
-                    0:{
-                        items:1
-                    },
-                    600:{
-                        items:2
-                    },
-                    1000:{
-                        items:3
-                    }
-                }
-            });
-        });
-    </script>
-
+<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('js/carrousel.js') }}"></script>
 <script>
     AOS.init();
 </script>
