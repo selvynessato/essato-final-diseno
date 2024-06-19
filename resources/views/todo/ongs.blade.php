d18ead93297b75621c96a8584d6470f030cd0407
Selvyn Quijivix <selvyn.essato@gmail.com>
2024-06-19 17:13:22 -0600

    novena parte voy a la mitad

--- resources/views/todo/ongs.blade.php
+++ resources/views/todo/ongs.blade.php
@@ -75,7 +75,7 @@
                             <hr class="hr-blanca">
                         </div>
                         <div class="d-grid gap-2 col-6 mx-auto boton-wha">
-                            <button class="btn btn-light" type="button">Whatsapp</button>
+                            <a href="https://wa.link/a4qy4l" class="btn btn-light" role="button" target="_blank">Whatsapp</a>
                         </div>
                     </div>
                 </div>
@@ -84,7 +84,7 @@
     </div>
 </div>
 <div class="tercera-parte">
-    <div class="container-fluid">
+    <div class="container">
         <div class="row align-items-start">
             <div class="col-md-8 col-12">
                 <h1 class="display-1 tercera-texto-izquierdo">DISEÑO WEB</h1>
@@ -101,6 +101,169 @@
                 <h1 class="display-1 tercera-texto-derecha">REDES SOCIALES</h1>
             </div>
         </div>           
+        <div class="row align-items-start">
+            <div class="col-md-11 col-12">
+                <h3 class="tercera-texto-centro">POSICIONAMIENTO DE MARCAS</h3>
+            </div>
+        </div>           
+    </div>
+</div>
+<div class="cuarta-parte">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="{{asset('/images/ongs/asokape.png')}}" class="img-fluid parte-cuatro-img" alt="empresa web">
+            </div>
+            <div class="col-sm-3">
+                <img src="{{asset('/images/ongs/ods-15.png')}}" class="img-fluid parte-cuatro-imguno" alt="ods diseño">
+            </div>
+            <div class="col-sm-3">
+                <img src="{{asset('/images/ongs/redes-sociales.png')}}" class="img-fluid parte-cuatro-imgdos" alt="red social">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="quinta-parte">
+    <div class="container">
+        <div class="row align-items-start">
+            <div class="col-md-3 col-12">
+                <h1 class="display-1 quinta-texto-izquierdo">+10</h1>
+            </div>
+            <div class="col-md-3 col-12">
+                <h1 class="display-6 quinta-texto-anio">AÑOS</h1>
+                <h1 class="display-5 quinta-texto-experiencia">EXPERIENCIA</h1>
+            </div>
+        </div>  
+    </div>
+</div>
+
+<div class="sexta-parte">
+    <div class="container">
+        <div class="row align-items-start">
+            <div class="col-md-3 col-12"></div>
+            <div class="col-md-6 col-12">
+                <h2 class="sexta-texto-titulo">Somos una agencia de progreso y avance de ONGs</h2>
+            </div>
+            <div class="col-md-3 col-12"></div>
+        </div>  
+        <div class="row align-items-start">
+            <div class="col-md-3 col-12"></div>
+            <div class="col-md-6 col-12">
+                <p class="parrajo-sexta-parte">
+                    Nos dedicamos a ayudar a las ONGs a alcanzar sus objetivos, maximizar su impacto y conectar con sus audiencias de manera efectiva, asegurando que sus historias y misiones sean vistas y valoradas por el mundo.
+                </p>
+            </div>
+            <div class="col-md-3 col-12"></div>
+        </div>  
+    </div>
+</div>
+<div class="septima-parte">
+    <div class="container">
+        <div class="row align-items-start">
+            <div class="col-md-6 col-12">
+                <p class="septimo-parrafo-titulo">
+                    Potenciamos <span class="parrafo-negrita">la sostenibillidad y visibilidad </span>de sus acciones
+                </p>
+            </div>
+            <div class="col-md-6 col-12">
+                <div class="d-grid gap-2 col-6">
+                    <a href="https://wa.link/a4qy4l" class="btn btn-light boton-w" role="button" target="_blank">Whatsapp</a>
+                </div>        
+            </div>
+        </div>  
+    </div>
+</div>
+<div class="octava-parte">
+    <div class="owl-carousel owl-theme">
+        <div class="item">                 
+            <div class="card">
+                <div class="card-header">
+                    <h3>
+                        Marketing
+                    </h3>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        llevemos a tu cliente potencial justo donde quieres que esté: a las puertas de tu negocio.  
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="item">                 
+            <div class="card">
+                <div class="card-header">
+                    <h3>
+                        Redes Sociales
+                    </h3>
+                </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            Creemos conexiones sólidas con tu audiencia y elevemo la percepción de tu marca.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        <div class="item">                 
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>
+                                Desarrollo web
+                            </h3>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                Diseñemos un espacio digital que cautiva a tus visitantes y los convierte en clientes fieles.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">                 
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>
+                                Branding
+                            </h3>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                Construyamos una imagen sólida definiendo el valor de tu marca.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">                 
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>
+                                Asesoría financiera
+                            </h3>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                Ordenemos tus números, gestionando ingresos, salidas y gastos para garantizar la estabilidad y el crecimiento continuo de tu negocio.
+                            </p>
+                        </div>
+                        <div class="card-footer">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+</div>
+<div class="novena-parte">
+    <div class="container">
+        <div class="row align-items-start">
+            <div class="col-md-2 col-12"></div>
+            <div class="col-md-4 col-12">
+                <img src="{{asset('/images/ongs/teresa-branding.png')}}" class="img-fluid parte-novena-img" alt="branding">
+                <h2 class="empresa-tituloUno">Empresa</h2>
+                <p class="empresa-subtituloUno">que se trabajo</p>
+            </div>
+            <div class="col-md-4 col-12">
+                <video src="{{asset('/images/ongs/teresa-video.mp4')}}" class="object-fit-contain video-teresa" autoplay muted playsinline></video>
+            </div>
+            <div class="col-md-2 col-12"></div>
+        </div>  
     </div>
 </div>
 </main>
